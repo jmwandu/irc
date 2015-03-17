4e0e71c91047cbe7dc07f190a68c51cc4f143c89
jmwandu <jmwandu@mail.umw.edu>
2015-03-17 19:08:52 +0000

    finsihed the project

--- server.py
+++ server.py
@@ -1,15 +1,26 @@
 import os
 import uuid
-from flask import Flask, session
+import psycopg2, psycopg2.extras
+from flask import Flask, session, render_template
 from flask.ext.socketio import SocketIO, emit
 
 app = Flask(__name__, static_url_path='')
-app.config['SECRET_KEY'] = 'secret!'
-
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+app.debug = True
 socketio = SocketIO(app)
 
-messages = [{'text':'test', 'name':'testName'}]
+messages = []
 users = {}
+
+
+
+
+def connectToDB():
+  connectionString = 'dbname=session user=postgres password=postgres host=localhost'
+  try:
+    return psycopg2.connect(connectionString)
+  except:
+    print("Can't connect to database")
 
 def updateRoster():
     names = []
@@ -23,6 +34,7 @@ def updateRoster():
     emit('roster', names, broadcast=True)
     
 
+
 @socketio.on('connect', namespace='/chat')
 def test_connect():
     session['uuid']=uuid.uuid1()
@@ -31,9 +43,18 @@ def test_connect():
     
     users[session['uuid']]={'username':'New User'}
     updateRoster()
-
-
+    
+    conn = connectToDB()
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    fetch_messages = "select text, username from messages join users on users.id = messages.user_id"
+    cur.execute(fetch_messages)
+    messages = cur.fetchall()
+    
+    keys = ['text', 'name']
+    
     for message in messages:
+        message = dict(zip(keys,message))
+        print(message)
         emit('message', message)
 
 @socketio.on('message', namespace='/chat')
@@ -41,6 +62,13 @@ def new_message(message):
     #tmp = {'text':message, 'name':'testName'}
     tmp = {'text':message, 'name':users[session['uuid']]['username']}
     messages.append(tmp)
+    conn = connectToDB()
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    print ("I'm here friend!")
+    message_insert = "INSERT INTO messages VALUES (default, %s, %s)";
+    cur.execute(message_insert, (message, session['id']))
+    print ("come at me bro!")
+    conn.commit()
     emit('message', tmp, broadcast=True)
     
 @socketio.on('identify', namespace='/chat')
@@ -49,12 +77,42 @@ def on_identify(message):
     users[session['uuid']]={'username':message}
     updateRoster()
 
+@socketio.on('search', namespace='/chat')
+def on_search(search):
+    print 'search: ' 
+    search = '%' + search + '%'
+    conn = connectToDB()
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    search_query = "select username, text from messages join users on messages.user_id = users.id where text like %s or username like %s"
+    cur.execute(search_query, (search, search))
+    results = cur.fetchall()
+    keys = ['name', 'text']
+    
 
+    emit('clearResults', {})    
+    for result in results:
+    
+        emit('search', dict(zip(keys,result)))
+        
 @socketio.on('login', namespace='/chat')
-def on_login(pw):
-    print 'login '  + pw
-    #users[session['uuid']]={'username':message}
-    #updateRoster()
+def on_login(data):
+    print 'omg lol'
+    print 'login '  + data['password']
+    print 'login' +data['username']
+  
+    
+    conn = connectToDB()
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    login_query = "select * from users where username = %s AND password = crypt(%s, password)"
+    print(cur.mogrify(login_query, (data['username'], data['password'])))
+    cur.execute(login_query, (data['username'], data['password']))
+    result = cur.fetchone()
+    if result:
+        users[session['uuid']]={'username': data['username']}
+        session['username'] = data['username']
+        session['id'] = result['id']
+        print 'successful login'
+        updateRoster()
 
 
     
@@ -68,8 +126,9 @@ def on_disconnect():
 @app.route('/')
 def hello_world():
     print 'in hello world'
-    return app.send_static_file('index.html')
-    return 'Hello World!'
+    #return app.send_static_file('index.html')
+    return render_template('index.html')
+
 
 @app.route('/js/<path:path>')
 def static_proxy_js(path):
